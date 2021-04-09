fb60cd2fe14821e087efe581ca14e30311db2fa3
DarkVoid6715 <siva.jai.tanikonda@gmail.com>
2021-04-09 12:38:04 -0700

    Tweaked stress_tester_template.sh

--- Resources/Prewritten Code/stress_tester_template.sh
+++ Resources/Prewritten Code/stress_tester_template.sh
@@ -1,15 +1,15 @@
 #STRESS-TESTER TEMPLATE
 #Purpose: This is just a Bash template for a stress tester
 
-g++ -std=c++17 -Wall -fsanitize=address -fsanitize=undefined -Wno-pragmas -Wno-attributes -D_GLIBCXX_DEBUG -o a.out A.cpp
-g++ -std=c++17 -Wall -fsanitize=address -fsanitize=undefined -Wno-pragmas -Wno-attributes -D_GLIBCXX_DEBUG -o b.out B.cpp
-#g++ -std=c++17 -Wall -fsanitize=address -fsanitize=undefined -Wno-pragmas -Wno-attributes -D_GLIBCXX_DEBUG -o c.out C.cpp
+g++ -std=c++17 -Wall -fsanitize=address -fsanitize=undefined -Wno-pragmas -Wno-attributes -D_GLIBCXX_DEBUG -o A_compiled.out A.cpp
+g++ -std=c++17 -Wall -fsanitize=address -fsanitize=undefined -Wno-pragmas -Wno-attributes -D_GLIBCXX_DEBUG -o B_compiled.out B.cpp
+#g++ -std=c++17 -Wall -fsanitize=address -fsanitize=undefined -Wno-pragmas -Wno-attributes -D_GLIBCXX_DEBUG -o C_compiled.out C.cpp
 echo START
 for((i = 1; i <= 100; i++)); do
     echo test $i 
-    ./a.out > input.txt
-    ./b.out < input.txt > B.txt
-    #./c.out < input.txt > C.txt
-    #diff -w B.txt C.txt || break
+    ./A_compiled.out > input.txt
+    ./B_compiled.out < input.txt > B_output.txt
+    #./C_compiled.out < input.txt > C_output.txt
+    #diff -w B_output.txt C_output.txt || break
 done
 echo DONE
