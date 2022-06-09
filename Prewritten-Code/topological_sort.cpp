dfde6c993ef000dd9bcea7e097b9459ebe9dbd53
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2022-06-08 20:50:33 -0700

    Reformatted Topological Sort Code

--- Prewritten-Code/topological_sort.cpp
+++ Prewritten-Code/topological_sort.cpp
@@ -10,23 +10,23 @@ struct topological_sort{
     vector<int> in;
     void init(int n, vector<int> inp[]){
         adj = vector<vector<int>>(n + 1, vector<int>());
-        for (int i = 1; i <= n; i++) adj[i] = inp[i];
+        for(int i = 1; i <= n; i++) adj[i] = inp[i];
     }
     void init(vector<vector<int>> inp){ adj = inp; }
     vector<int> getSort(){
         queue<int> que;
         vector<int> ord;
         in = vector<int>(adj.size());
-        for (int i = 1; i < (int)adj.size(); i++)
-            for (int j : adj[i]) in[j]++;
-        for (int i = 1; i < (int)adj.size(); i++)
-            if (!in[i]) que.push(i);
-        while (!que.empty()){
+        for(int i = 1; i < (int)adj.size(); i++)
+            for(int j : adj[i]) in[j]++;
+        for(int i = 1; i < (int)adj.size(); i++)
+            if(!in[i]) que.push(i);
+        while(!que.empty()){
             int vtx = que.front();
             que.pop();
             ord.push_back(vtx);
-            for (int i : adj[vtx])
-                if (--in[i] <= 0) que.push(i);
+            for(int i : adj[vtx])
+                if(--in[i] <= 0) que.push(i);
         }
         return ord;
     }
