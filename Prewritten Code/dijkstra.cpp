53e8f06229e32183217c13ed1d27c3126a2e78f8
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2022-07-06 12:31:50 -0700

    Added Dijkstra Algorithm Code

---
+++ Prewritten Code/dijkstra.cpp
@@ -0,0 +1,36 @@
+/*
+DIJKSTRA'S ALGORITHM
+Purpose: Find the shortest path from one node to all other nodes on a graph (without negative edges)
+Time Complexity: O(Mlg(N))
+Space Complexity: O(M + N)
+*/
+
+template<class T> struct dijkstra {
+    vector<vector<pair<int, T>>> adj;
+    vector<T> dst;
+    vector<int> pre;
+    void init(int n, vector<pair<int, T>> inp[]) {
+        adj = vector<vector<pair<int, T>>>(n + 1, vector<pair<int, T>>());
+        for (int i = 1; i <= n; i++) adj[i] = inp[i];
+    }
+    void init(vector<vector<pair<int, T>>> &inp) { adj = inp; }
+    vector<T> getPaths(int src) {
+        dst = vector<T>(adj.size(), -1);
+        pre = vector<int>(adj.size());
+        set<pair<T, int>> que;
+        que.emplace(0, src);
+        dst[src] = 0;
+        while (!que.empty()) {
+            int vtx = que.begin()->second;
+            que.erase(que.begin());
+            for (auto &i : adj[vtx])
+                if (dst[i.first] == -1 || dst[i.first] > dst[vtx] + i.second) {
+                    que.erase({ dst[i.first], i.first });
+                    dst[i.first] = dst[vtx] + i.second;
+                    pre[i.first] = vtx;
+                    que.emplace(dst[i.first], i.first);
+                }
+        }
+        return dst;
+    }
+};
