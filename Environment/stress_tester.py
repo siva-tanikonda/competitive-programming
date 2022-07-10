da38521ece3b4f327d212d81f9a28d56112c0833
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2022-07-10 14:12:52 -0700

    Created build folder in environments

--- Environment/stress_tester.py
+++ Environment/stress_tester.py
@@ -1,6 +1,7 @@
 import os
 import filecmp
 import sys
+import time
 
 def printFileContents(name):
     file = open(name, "r")
@@ -23,9 +24,9 @@ directoryBase = sys.argv[3]
 print("Beginning process...")
 
 #These are just the build commands
-os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + fileBase + "_generator_compiled -g " + fileBase + "_generator.cpp")
-os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + fileBase + "_compiled -g " + fileBase + ".cpp")
-os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + fileBase + "_slow_compiled -g " + fileBase + "_slow.cpp")
+os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "build\\" + fileBase + "_generator_compiled -g " + fileBase + "_generator.cpp")
+os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "build\\" + fileBase + "_compiled -g " + fileBase + ".cpp")
+os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "build\\" + fileBase + "_slow_compiled -g " + fileBase + "_slow.cpp")
 print("Building complete...")
 
 #Change this if you want to run more or fewer tests
@@ -34,9 +35,9 @@ n = 100
 print("Beginning tests...")
 for i in range(1, n + 1):
     print("Test " + str(i) + ":", end = " ")
-    os.system(fileBase + "_generator_compiled > \"" + directoryBase + "\\input.txt\"")
-    os.system(fileBase + "_compiled < \"" + directoryBase + "\\input.txt\" > " + fileBase + "_output.txt")
-    os.system(fileBase + "_slow_compiled < \"" + directoryBase + "\\input.txt\" > " + fileBase + "_slow_output.txt")
+    os.system("build\\" + fileBase + "_generator_compiled > \"" + directoryBase + "\\input.txt\"")
+    os.system("build\\" + fileBase + "_compiled < \"" + directoryBase + "\\input.txt\" > " + fileBase + "_output.txt")
+    os.system("build\\" + fileBase + "_slow_compiled < \"" + directoryBase + "\\input.txt\" > " + fileBase + "_slow_output.txt")
     res = filecmp.cmp(fileBase + "_output.txt", fileBase + "_slow_output.txt")
     if (not res):
         print(color.RED + "FAILED" + color.END)
