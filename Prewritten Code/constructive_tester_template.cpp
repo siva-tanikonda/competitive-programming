9ac4f127b152db648a2062a83b8bec7e9ac62820
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2022-08-28 09:32:19 -0700

    Created constructive tester template

---
+++ Prewritten Code/constructive_tester_template.cpp
@@ -0,0 +1,25 @@
+/*
+CONSTRUCTIVE TESTER TEMPLATE
+Purpose: This is just my template to check solutions to constructive problems
+*/
+
+#pragma GCC optimize("unroll-loops, O3")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+using ld = long double;
+const int xx[8] = { -1, 0, 1, 0, -1, -1, 1, 1 };
+const int yy[8] = { 0, -1, 0, 1, -1, 1, -1, 1 };
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), srand(time(NULL));
+    freopen("input.txt", "r", stdin);
+    
+    freopen("code_output.txt", "r", stdin);
+
+    return 0;
+}
