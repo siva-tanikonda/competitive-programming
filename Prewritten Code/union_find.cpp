25548dd3d18bc649e90a77cc5cea5c5286b7341a
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2022-09-02 19:54:43 -0700

    Altered spacing on union_find.cpp

--- Prewritten Code/union_find.cpp
+++ Prewritten Code/union_find.cpp
@@ -5,17 +5,17 @@ Time Complexity: [Merge]O(T(n)) [Find]O(T(n))
 Space Complexity: O(n)
 */
 
-struct union_find{
+struct union_find {
     vector<int> par;
     void init(int n) { par.resize(n, -1); }
     int size(int x) { return -par[find(x)]; }
-    int find(int x){
+    int find(int x) {
         return (par[x] < 0) ? x : par[x] = find(par[x]);
     }
-    bool unite(int a, int b){
+    bool unite(int a, int b) {
         a = find(a), b = find(b);
-        if(a == b) return false;
-        if(size(a) < size(b)) swap(a, b);
+        if (a == b) return false;
+        if (size(a) < size(b)) swap(a, b);
         par[a] += par[b], par[b] = a;
         return true;
     }
