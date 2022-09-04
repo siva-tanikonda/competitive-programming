06a3fbbf3a11899dd80d84ff19e5551277017e7e
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2022-09-04 11:26:11 -0700

    Added Fenwick Tree code to snippets

--- Prewritten Code/fenwick_tree.cpp
+++ Prewritten Code/fenwick_tree.cpp
@@ -5,16 +5,16 @@ Time Complexity: [Update]O(lg(n)) [Query]O(lg(n))
 Space Complexity: O(n)
 */
 
-template<class T> struct fenwick_tree{
+template <class T> struct fenwick_tree {
     vector<T> arr;
-    void init(int n) { arr.resize(n); }
-    void update(int pos, T add){
-        for(; pos < (int)arr.size(); pos |= pos + 1)
+    void init(int n) { arr = vector<int>(n + 1, 0); }
+    void update(int pos, T add) {
+        for (; pos < (int)arr.size(); pos |= pos + 1)
             arr[pos] += add;
     }
-    T query(int pos){
+    T query(int pos) {
         T sum = 0;
-        for(; pos >= 0; pos = (pos & (pos + 1)) - 1)
+        for (; pos >= 0; pos = (pos & (pos + 1)) - 1)
             sum += arr[pos];
         return sum;
     }
