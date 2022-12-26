14fcadf5a5aba619188bc6629991849c96c3a7f5
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2022-12-26 08:40:29 -0800

    Created factorials class

--- Prewritten Code/factorials.cpp
+++ Prewritten Code/factorials.cpp
@@ -1,27 +1,37 @@
 /*
-FACTORIALS
+FACTORIALS CLASS
 Purpose: This is just a basic class for maintaining factorials modded by a certain number
 Time Complexity: [Build]O(nlg(n)) [Query]O(1)
 Space Complexity: O(n)
 */
 
-struct factorials {
+struct factorials_class {
+
     vector<int> fac, inv;
+
+    //Calculates the factorials and the modular inverses of the factorials
     void init(int n) {
         fac = vector<int>(n + 1);
         inv = vector<int>(n + 1);
         fac[0] = inv[0] = 1;
         for (int i = 1; i <= n; i++) {
-            fac[i] = mod.mult(fac[i - 1], i);
+            fac[i] = mod.mul(fac[i - 1], i);
             inv[i] = mod.inv(fac[i]);
         }
     }
+    //Gives us x!
+    int query(int x) { return fac[x]; }
+    
+    //Gives us xPy
     int prm(int x, int y) {
         if (y > x) return 0;
-        return mod.mult(fac[x], inv[x - y]);
+        return mod.mul(fac[x], inv[x - y]);
     }
+
+    //Gives us x choose y
     int cmb(int x, int y) {
         if (y > x) return 0;
-        return mod.mult(prm(x, y), inv[y]);
+        return mod.mul(prm(x, y), inv[y]);
     }
+    
 };
