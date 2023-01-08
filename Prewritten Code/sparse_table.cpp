abc1462b06153075aaf611c112536adedb5436f0
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2023-01-08 14:00:41 -0800

    Sparse Table Implementation Added

--- Prewritten Code/sparse_table.cpp
+++ Prewritten Code/sparse_table.cpp
@@ -5,8 +5,18 @@ Time Complexity: [Build]O(nlg(n)) [Query]O(1)
 Space Complexity: O(nlg(n))
 */
 
-template<class T> struct sparse_table{
+template<class T> struct sparse_table {
+
     vector<vector<T>> jmp;
+
+    //Initializes for static length arrays
+    void init(int n, T a[]) {
+        vector<T> arr(n);
+        for (int i = 0; i < n; i++) arr[i] = a[i];
+        init(arr);
+    }
+
+    //initializes for dynamic-length lists
     void init(vector<T> &arr){
         int n = (int)arr.size(), dpt = 32 - __builtin_clz(n);
         jmp = vector<vector<T>>(dpt, arr);
@@ -14,8 +24,11 @@ template<class T> struct sparse_table{
             for(int j = 0; j < n; j++)
                 jmp[i + 1][j] = min(jmp[i][j], jmp[i][min(j + (1 << i), n - 1)]);
     }
-    T query(int l, int r){
+
+    //queries a certain range
+    T query(int l, int r) {
         int dpt = 31 - __builtin_clz(r - l + 1);
         return min(jmp[dpt][l], jmp[dpt][r - (1 << dpt) + 1]);
     }
+
 };
