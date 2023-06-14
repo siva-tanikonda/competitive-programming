f94c6dc68599e82de23c470ddb665ee4b4bc56b5
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2023-06-14 10:34:09 -0700

    Updated stress tester code

--- Environment/stress_tester.py
+++ Environment/stress_tester.py
@@ -1,8 +1,6 @@
 import os
 import filecmp
-from re import L
 import sys
-import time
 
 #change this if you want to run more or fewer tests
 n = 100
@@ -33,33 +31,41 @@ def getFileContents(name):
 def stressTest():
 
     #You need arguments when running the scripts
-    fileBase = sys.argv[2]
-    directoryBase = sys.argv[3]
+    file_base = sys.argv[2]
+    directory_base = sys.argv[3]
+    operating_system = sys.argv[4]
+
+    #Pick file slash character based on OS
+    slash = ""
+    if (operating_system == "windows"):
+        slash = "\\"
+    elif (operating_system == "macos"):
+        slash = "/"
 
     print("Beginning process...")
 
     #These are just the build commands
-    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + fileBase + "_generator_compiled -g " + fileBase + "_generator.cpp")
-    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + fileBase + "_compiled -g " + fileBase + ".cpp")
-    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + fileBase + "_slow_compiled -g " + fileBase + "_slow.cpp")
+    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + file_base + "_generator_compiled -g " + file_base + "_generator.cpp")
+    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + file_base + "_compiled -g " + file_base + ".cpp")
+    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + file_base + "_slow_compiled -g " + file_base + "_slow.cpp")
     print("Building complete...")
 
     #Run the tests and check if both programs have the same output
     print("Beginning tests...")
     for i in range(1, n + 1):
         print("Test " + str(i) + ":", end = " ")
-        os.system("Build/" + fileBase + "_generator_compiled > \"" + directoryBase + "/input.txt\"")
-        os.system("Build/" + fileBase + "_compiled < \"" + directoryBase + "/input.txt\" > " + fileBase + "_output.txt")
-        os.system("Build/" + fileBase + "_slow_compiled < \"" + directoryBase + "/input.txt\" > " + fileBase + "_slow_output.txt")
-        res = filecmp.cmp(fileBase + "_output.txt", fileBase + "_slow_output.txt")
+        os.system("Build" + slash + file_base + "_generator_compiled > \"" + directory_base + "/input.txt\"")
+        os.system("Build" + slash + file_base + "_compiled < \"" + directory_base + "/input.txt\" > " + file_base + "_output.txt")
+        os.system("Build" + slash + file_base + "_slow_compiled < \"" + directory_base + "/input.txt\" > " + file_base + "_slow_output.txt")
+        res = filecmp.cmp(file_base + "_output.txt", file_base + "_slow_output.txt")
         if (not res):
             print(color.RED + "FAILED" + color.END)
             print(color.BOLD + "input.txt:", color.END)
-            printFileContents(directoryBase + "/input.txt")
-            print(color.BOLD + fileBase + "_output.txt:", color.END)
-            printFileContents(fileBase + "_output.txt")
-            print(color.BOLD + fileBase + "_slow_output.txt:", color.END)
-            printFileContents(fileBase + "_slow_output.txt")
+            printFileContents(directory_base + "/input.txt")
+            print(color.BOLD + file_base + "_output.txt:", color.END)
+            printFileContents(file_base + "_output.txt")
+            print(color.BOLD + file_base + "_slow_output.txt:", color.END)
+            printFileContents(file_base + "_slow_output.txt")
             break
         else:
             print(color.GREEN + "SUCCESS" + color.END)
@@ -68,32 +74,40 @@ def stressTest():
 def constructiveTest():
 
     #You need arguments when running the scripts
-    fileBase = sys.argv[2]
-    directoryBase = sys.argv[3]
+    file_base = sys.argv[2]
+    directory_base = sys.argv[3]
+    operating_system = sys.argv[4]
+
+    #Pick file slash character based on OS
+    slash = ""
+    if (operating_system == "windows"):
+        slash = "\\"
+    elif (operating_system == "macos"):
+        slash = "/"
 
     print("Beginning process...")
 
     #These are just the build commands
-    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + fileBase + "_generator_compiled -g " + fileBase + "_generator.cpp")
-    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + fileBase + "_compiled -g " + fileBase + ".cpp")
-    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + fileBase + "_checker_compiled -g " + fileBase + "_checker.cpp")
+    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + file_base + "_generator_compiled -g " + file_base + "_generator.cpp")
+    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + file_base + "_compiled -g " + file_base + ".cpp")
+    os.system("g++ -std=c++17 -fdiagnostics-color=always -Wall -Wno-pragmas -Wno-attributes -o " + "Build/" + file_base + "_checker_compiled -g " + file_base + "_checker.cpp")
     print("Building complete...")
 
     #Run the tests and check if each output is a proper construction based-off the input
     print("Beginning tests...")
     for i in range(1, n + 1):
         print("Test " + str(i) + ":", end = " ")
-        os.system("Build/" + fileBase + "_generator_compiled > \"" + directoryBase + "/input.txt\"")
-        os.system("Build/" + fileBase + "_compiled < \"" + directoryBase + "/input.txt\" > " + fileBase + "_output.txt")
-        os.system("Build/" + fileBase + "_checker_compiled > " + fileBase + "_checker_output.txt")
-        verdict = getFileContents(fileBase + "_checker_output.txt")
+        os.system("Build" + slash + file_base + "_generator_compiled > \"" + directory_base + "/input.txt\"")
+        os.system("Build" + slash + file_base + "_compiled < \"" + directory_base + "/input.txt\" > " + file_base + "_output.txt")
+        os.system("Build" + slash + file_base + "_checker_compiled > " + file_base + "_checker_output.txt")
+        verdict = getFileContents(file_base + "_checker_output.txt")
         valid = (verdict == "yes")
         if (not valid):
             print(color.RED + "FAILED" + color.END)
             print(color.BOLD + "input.txt:", color.END)
-            printFileContents(directoryBase + "/input.txt")
-            print(color.BOLD + fileBase + "_output.txt:", color.END)
-            printFileContents(fileBase + "_output.txt")
+            printFileContents(directory_base + "/input.txt")
+            print(color.BOLD + file_base + "_output.txt:", color.END)
+            printFileContents(file_base + "_output.txt")
             break
         else:
             print(color.GREEN + "SUCCESS" + color.END)
