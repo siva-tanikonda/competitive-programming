878b11dbcda41eeefc03467f04e416949ab2a596
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2023-06-14 08:35:25 -0700

    Slight optimization to matrix operations class

--- Prewritten Code/matrix_operations.cpp
+++ Prewritten Code/matrix_operations.cpp
@@ -6,7 +6,7 @@ Purpose: This is just a class that supports basic matrix operations
 struct matrix_operations {
 
     //Element-wise addition of two matrices (Time Complexity: O(nm))
-    vector<vector<ll>> add(vector<vector<ll>> a, vector<vector<ll>> b) {
+    vector<vector<ll>> add(vector<vector<ll>> &a, vector<vector<ll>> &b) {
         vector<vector<ll>> res(a.size(), vector<ll>(a[0].size()));
         for (int i = 0; i < (int)a.size(); i++)
             for (int j = 0; j < (int)a[0].size(); j++)
@@ -15,7 +15,7 @@ struct matrix_operations {
     }
 
     //Element-wise subtraction of two matrices (Time Complexity: O(nm))
-    vector<vector<ll>> sub(vector<vector<ll>> a, vector<vector<ll>> b) {
+    vector<vector<ll>> sub(vector<vector<ll>> &a, vector<vector<ll>> &b) {
         vector<vector<ll>> res(a.size(), vector<ll>(a[0].size()));
         for (int i = 0; i < (int)a.size(); i++)
             for (int j = 0; j < (int)a[0].size(); j++)
@@ -24,7 +24,7 @@ struct matrix_operations {
     }
 
     //Typical matrix multiplication (Time Complexity: O(nmk))
-    vector<vector<ll>> mul(vector<vector<ll>> a, vector<vector<ll>> b) {
+    vector<vector<ll>> mul(vector<vector<ll>> &a, vector<vector<ll>> &b) {
         vector<vector<ll>> res(a.size(), vector<ll>(b[0].size()));
         for (int i = 0; i < (int)a.size(); i++)
             for (int j = 0; j < (int)b[0].size(); j++)
@@ -41,7 +41,7 @@ struct matrix_operations {
     }
 
     //Exponentiates a square matrix (binary exponentiation) (Time Complexity: O(m^3log(n)))
-    vector<vector<ll>> pow(vector<vector<ll>> x, ll n) {
+    vector<vector<ll>> pow(vector<vector<ll>> &x, ll n) {
         if (!n) return identity(x.size());
         vector<vector<ll>> res = pow(x, n / 2);
         res = mul(res, res);
