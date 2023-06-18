a1305056d09eb3740b79f3e18466ef2ef83b3bdc
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2023-06-17 18:06:55 -0700

    Added PI constant to templates

--- Prewritten Code/multiple_testcases_template.cpp
+++ Prewritten Code/multiple_testcases_template.cpp
@@ -4,28 +4,27 @@ Purpose: This is just my default template for problems with multiple testcases i
 */
 
 #pragma GCC optimize("unroll-loops, O3")
-#pragma GCC target("avx")
 #include <bits/stdc++.h>
 #include <ext/pb_ds/tree_policy.hpp>
 #include <ext/pb_ds/assoc_container.hpp>
-#define IO(i, o) freopen(i, "r", stdin), freopen(o, "w", stdout)
 using namespace std;
 using namespace __gnu_pbds;
 using ll = long long;
 using ld = long double;
-const int xx[8] = {-1, 0, 1, 0, -1, -1, 1, 1};
-const int yy[8] = {0, -1, 0, 1, -1, 1, -1, 1};
+template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int xx[8] = { -1, 0, 1, 0, -1, -1, 1, 1 };
+const int yy[8] = { 0, -1, 0, 1, -1, 1, -1, 1 };
+const ld pi = atan(1) * 4;
 
-void solve(){
+void solve() {
 
 }
 
-int main(){
-    //IO("input.txt", "output.txt");
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), srand(time(NULL));
     int t;
     cin >> t;
-    while(t--) solve();
+    while (t--) solve();
     return 0;
 }
