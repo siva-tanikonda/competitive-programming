ce5167fe67aa46aa3aaa9f2f5e4f311c1d62169d
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2024-09-28 10:50:02 -0700

    Updated binary search code

--- Prewritten Code/binary_search.cpp
+++ Prewritten Code/binary_search.cpp
@@ -1,16 +1,17 @@
 /*
 BINARY SEARCH
 Purpose: Finds the index of an element in a sorted list
-Time Complexity: O(log(n))
+Time Complexity: O(log(N))
 Space Complexity: O(1)
 */
 
-template<class T> T binary_search(vector<T> &a, T val) {
-    int n = a.size();
+template<class T>
+T binary_search(vector<T> &arr, T val) {
+    int n = arr.size();
     T l = 0, r = n - 1;
     while (l < r) {
         int mid = l + (r - l) / 2;
-        if (val <= a[mid]) r = mid;
+        if (val <= arr[mid]) r = mid;
         else l = mid + 1;
     }
     return (a[l] == val) ? l : -1;
