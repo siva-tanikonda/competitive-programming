8c099dae8c4f0c3ec23b3b266c97dce17fa6ceb1
Siva Tanikonda <siva.jai.tanikonda@gmail.com>
2025-08-30 19:19:55 -0700

    ...

--- Prewritten Code/extended_gcd.cpp
+++ Prewritten Code/extended_gcd.cpp
@@ -1,11 +1,11 @@
 /*
 EXTENDED GCD
-Purpose: This gives one of the solutions to an equation of the form: ax + by = gcd(a, b)
+Purpose: This gives one of the integer solutions to an equation of the form: ax + by = gcd(a, b)
 Time Complexity: O(log(N))
 Space Complexity: O(1)
 */
 
-template <class T> tuple<T, T, T> extended_gcd(T a, T b){
+template <class T> tuple<T, T, T> extended_gcd(T a, T b) {
     if(b == 0) return make_tuple(1, 0, a);
     T x, y, g;
     tie(x, y, g) = extended_gcd(b, a % b);
